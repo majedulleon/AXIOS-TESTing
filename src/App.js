afbeac400a436445e9841df17227176722b9eccf
majedulleon <majedulleon@gmail.com>
2023-03-05 21:51:48 +0600

    All AXIOS EXAMPLES

--- src/App.js
+++ src/App.js
@@ -11,7 +11,21 @@ class App extends Component {
     // fetch('https://jsonplaceholder.typicode.com/posts')
     //   .then(response => response.json()).then(data => console.log(data))
 
-    //AXIOS API EXAMPLE
+    //AXIOS GET EXAMPLE
+    // axios.get('https://jsonplaceholder.typicode.com/posts')
+    //   .then(response => response.data)
+    //   .then(data => console.log(data))
+    //   .catch(error => console.log(error.message));
+
+    //AXIOS POST EXAMPLE
+    // axios.post('https://jsonplaceholder.typicode.com/posts', { title: "Something", body: "Something Else" }).then(response => console.log(response));
+
+    //AXIOS PUT EXAMPLE (/2 is id)
+    // axios.put('https://jsonplaceholder.typicode.com/posts/2', { title: "Something1", body: "Something Else1" }).then(response => console.log(response));
+
+    //AXIOS DELETE EXAMPLE (/1 is id)
+    // axios.delete('https://jsonplaceholder.typicode.com/posts/1')
+    //   .then(response => console.log(response));
 
 
   }
