6826f96a81bcc41e50399da6de68865c48537969
majedulleon <majedulleon@gmail.com>
2023-03-05 21:13:29 +0600

    fetch api example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import fetch from 'cross-fetch';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount() {
+
+    //FETCH API EXAMPLE
+    // fetch('https://jsonplaceholder.typicode.com/posts')
+    //   .then(response => response.json()).then(data => console.log(data))
+
+    //AXIOS API EXAMPLE
+
+
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
